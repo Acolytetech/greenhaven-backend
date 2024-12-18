385ff476fb6dbf7deeb434e02ec032d0b829a2cc
rajacolyte <rajkamal@startupflora.com>
2024-12-18 17:49:48 +0530

    added AllCategory Schema

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -8,11 +8,12 @@ import { Footer } from "./Footer";
 import { Herosection } from "./Herosection";
 import { Navbar } from "./Navbar";
 import { Ourproducts } from "./Ourproducts";
+import { AllCategory } from "./product_category/AllCategory";
 import { Subscription } from "./Subscription";
 import { Subscriptiontext } from "./Subscriptioncontent";
 import { Testimonial } from "./Testimonial";
 import { Whychooseus } from "./whychooes";
 
 export const schemaTypes = [Navbar,Herosection,Whychooseus ,Testimonial,Subscriptiontext,Ourproducts,Subscription,Footer,
-    about,contactForm,fourthAbout,SecondAbout,thirdAbout,
+    about,contactForm,fourthAbout,SecondAbout,thirdAbout,AllCategory
 ]
