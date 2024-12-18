8ddebd7110ffaa17241d39db7683cef8ab639d83
Acolytetech <slawaniya03@gmail.com>
2024-12-18 17:57:19 +0530

    abouts us

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,9 +1,8 @@
-import { schematypes } from "./about";
 import about from "./about/about";
 import contactForm from "./about/contactForm";
 import fourthAbout from "./about/fourthAbout";
 import SecondAbout from "./about/SecondAbout";
-import thirdAbout from "./about/thirdAbout";
+// import thirdAbout from "./about/thirdAbout";
 import { Footer } from "./Footer";
 import { Herosection } from "./Herosection";
 import { Navbar } from "./Navbar";
@@ -14,5 +13,5 @@ import { Testimonial } from "./Testimonial";
 import { Whychooseus } from "./whychooes";
 
 export const schemaTypes = [Navbar,Herosection,Whychooseus ,Testimonial,Subscriptiontext,Ourproducts,Subscription,Footer,
-    about,contactForm,fourthAbout,SecondAbout,thirdAbout,
+    about,SecondAbout,fourthAbout,contactForm
 ]
